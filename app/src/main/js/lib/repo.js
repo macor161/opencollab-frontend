c12c5d807ad40e745407ab3351f4409e1bc2ef78
Mathew Cormier <mathew.corm@gmail.com>
2018-02-08 21:36:56 -0500

    createRepo

--- app/src/main/js/lib/repo.js
+++ app/src/main/js/lib/repo.js
@@ -1,5 +1,11 @@
 import fs from 'fs-extra'
+import opencollab from 'opencollab-lib'
 
 export function createRepo(opts) {
-
+    let repoPath = `tmp/repos/${opts.name}.git/.git`
+    return fs.mkdirp(repoPath)
+            .then(() => opencollab.init(repoPath))
+            .then(result => {
+                console.log('Repo created at: ', result)
+            })
 }
