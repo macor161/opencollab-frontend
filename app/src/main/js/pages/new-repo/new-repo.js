fdc5fc5cfa67506b588532a9bc721dfe0372629a
Mathew Cormier <mathew.corm@gmail.com>
2018-02-11 22:09:29 -0500

    Add materialize

--- app/src/main/js/pages/new-repo/new-repo.js
+++ app/src/main/js/pages/new-repo/new-repo.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import { Button, Input } from 'react-materialize'
 
 import './new-repo.css'
 
@@ -8,8 +9,16 @@ class NewRepoPage extends Component {
 
 	render(){
 		return(
-			<div className="new-repo">
-				<h1>Create a new Repo</h1>
+			<div className="new-repo page">
+                <div className="container-md">
+				    <h2>Create a new repository</h2>
+                    <p className="subhead-description">
+                        A repository contains all the files for your project, including the revision history.
+                    </p>
+                    <hr />
+
+                    <Input />
+                </div>
 			</div>
 		)
 	}
