5766e9b5405939a3caeef641d7dec7dfbfb86e8e
Mathew Cormier <mathew.corm@gmail.com>
2018-02-13 19:10:03 -0500

    Add clone dropdown to repo-page

--- app/src/main/js/pages/repo/repo-page-store.js
+++ app/src/main/js/pages/repo/repo-page-store.js
@@ -1,4 +1,5 @@
 import { observable, action } from 'mobx'
+import * as repos from '../../lib/repo'
 
 export const SECTION = {
     CODE: 'CODE',
@@ -12,7 +13,27 @@ export class RepoPageStore {
 
     @observable currentSection = SECTION.CODE
 
+    @observable repoStatus = {}
+
+    @observable dirName = ' '
+    @observable repoName = ''
+    @observable repoDescription = ''
+
     @action setCurrentSection(section) { this.currentSection = section }
+
+
+
+    constructor(name) {
+        this.dirName = name
+        this.init()
+    }
+
+    async init() {
+        this.repoStatus = await repos.status(this.dirName)
+
+        this.repoStatus.contract.getName().then(name => this.repoName = name)
+        this.repoStatus.contract.getDescription().then(desc => this.repoDescription = desc)
+    }
 }
 
 
--- app/src/main/js/pages/repo/repo-page.js
+++ app/src/main/js/pages/repo/repo-page.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 import { Link, withRouter } from 'react-router-dom'
 import { observer } from 'mobx-react'
-import { Button, Input, Preloader, Breadcrumb, MenuItem } from 'react-materialize'
+import { Button, Input, Preloader, Breadcrumb, MenuItem, Icon, Card } from 'react-materialize'
+import { Dropdown } from 'semantic-ui-react'
 import { default as c } from 'classnames'
-import {
-	HashRouter as Router,
-	Route,
-  } from 'react-router-dom'
+import { Router, Route } from 'react-router-dom'
+import { bind } from 'bind-decorator'
+
+import { history } from '../../lib/history'
 
 import { CodeSection } from './components/code-section/code-section'
 import { IssuesSection } from './components/issues-section/issues-section'
@@ -20,25 +21,51 @@ import './repo-page.css'
 class RepoPage extends Component {
 
   store
+  cloneDropdown
 
 
   constructor(props) {
     super(props)
-    this.store = new RepoPageStore()
+    this.store = new RepoPageStore(props.match.params.id)
+    this.state = { isCloneDropdownOpen: false }
 
     if (props.match.params.section) {
       this.store.setCurrentSection(props.match.params.section.toUpperCase().replace('-', '_'))
     }
+
+    window.repoPageStore = this.store
   }
 
-  selectSection(section) {
-    if (this.store.currentSection !== section)
-      this.props.history.push(`/repo/${this.props.match.params.id}/${section.toLowerCase().replace('_', '-')}`)
+  componentDidMount() {
+    document.addEventListener('mousedown', this.handleClickOutside)
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('mousedown', this.handleClickOutside)
   }
 
   componentWillUpdate(nextProps, nextState) {
     if (nextProps.match.params.section)
       this.store.setCurrentSection(nextProps.match.params.section.toUpperCase().replace('-', '_'))
+  }
+
+  @bind handleClickOutside(event) {
+    if (this.cloneDropdown && !this.cloneDropdown.ref.contains(event.target)) {
+      this.closeCloneDropdown()
+    }
+  }
+
+  selectSection(section) {
+    if (this.store.currentSection !== section)
+      history.push(`/repo/${this.props.match.params.id}/${section.toLowerCase().replace('_', '-')}`)
+  }
+
+  @bind openCloneDropdown() {
+    this.setState({ isCloneDropdownOpen: true })
+  }
+
+  @bind closeCloneDropdown() {
+    this.setState({ isCloneDropdownOpen: false })
   }
 
   render() {
@@ -47,8 +74,25 @@ class RepoPage extends Component {
         <div className="container">
           <Breadcrumb>
             <MenuItem>Repositories</MenuItem>
-            <MenuItem>{this.props.match.params.id}</MenuItem>
+            <MenuItem>{this.store.repoName}</MenuItem>
           </Breadcrumb>
+          
+          <p className="subhead-description">{this.store.repoDescription}</p>
+          <Dropdown 
+            floating 
+            ref={node => this.cloneDropdown = node}
+            className="clone-dropdown-container" 
+            icon=''
+            open={this.state.isCloneDropdownOpen}
+            trigger={<Button className="clone-btn" onClick={this.openCloneDropdown}>Clone or download <Icon small>arrow_drop_down</Icon></Button>}
+          >
+            <Dropdown.Menu>
+              <div className="clone-dropdown">
+                <h3>Clone with Mango <Icon>help_outline</Icon></h3>
+                <Input type="text" value={`mango://${this.store.repoStatus.mangoAddress}`} />
+              </div>
+            </Dropdown.Menu>
+          </Dropdown>
 
           <div className="tabs">
             <Button 
@@ -72,10 +116,10 @@ class RepoPage extends Component {
               Settings
             </Button>
           </div>
-          <Router>
+          <Router history={history}>
             <div>
-              <Route exact path="/repo/:id" component={CodeSection} />
-              <Route exact path="/repo/:id/code" component={CodeSection} />
+              <Route exact path="/repo/:id" render={() => <CodeSection repo={this.store.dirName} />} />
+              <Route exact path="/repo/:id/code" render={() => <CodeSection repo={this.store.dirName} />} />
               <Route exact path="/repo/:id/issues" component={IssuesSection} />
               <Route exact path="/repo/:id/pull-requests" component={PullRequestsSection} />
               <Route exact path="/repo/:id/settings" component={SettingsSection} />
@@ -87,4 +131,4 @@ class RepoPage extends Component {
   }
 }
 
-export default withRouter(RepoPage)
+export default RepoPage
