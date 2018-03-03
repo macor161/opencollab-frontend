7fa7a797485f323d226b9c132fe64bfa70894903
Mathew Cormier <mathew.corm@gmail.com>
2018-03-03 15:12:18 -0500

    Add extension loading to app

--- app/src/main/app.js
+++ app/src/main/app.js
@@ -1,50 +1,73 @@
-const electron = require('electron');
-// Module to control application life.
-const app = electron.app;
-// Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow;
-
+const { app, BrowserWindow, shell, session, Menu, ipcMain } = require('electron');
 const path = require('path');
 const url = require('url');
+const fs = require('fs');
 
-const {
-	default: installExtension, 
-	REACT_DEVELOPER_TOOLS
-} = require('electron-devtools-installer');
+const extensions = require('../../../extensions');
 
+let isDev;
+try {
+  isDev = require('electron-is-dev');
+} catch(e) {
+  isDev = false;
+}
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow;
+let win;
+
 
 /** This function will create the mainWindow */
 function createWindow() {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 1210, height: 768});
+	win = new BrowserWindow({width: 1210, height: 768});
+	
+	extensions.loadMetamask(session, win, isDev);
 
-	// and load the index.html of the app.
-	mainWindow.loadURL(url.format({
-		pathname: path.join(__dirname, 'index.html'),
-		protocol: 'file:',
-		slashes: true,
-	}));
+	let indexPath;
+  isDev ? indexPath = path.join(`brave/${__dirname}`, 'index.html') : indexPath = path.join(`brave/${__dirname}`, 'index.html');
+
+
+  setTimeout(() => {
+    win.loadURL(url.format({
+      pathname: indexPath,
+      protocol: 'chrome',
+      slashes: true
+    }));
+    var template = [{
+      label: "Edit",
+        submenu: [
+          { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+          { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+          { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" }
+      ]}
+    ];
+
+    //Menu.setApplicationMenu(Menu.buildFromTemplate(template));
+    //win.maximize();
+  }, 200);
+
+	ipcMain.on('open-link', (evt, link) => {
+    shell.openExternal(link);
+  });
 
   if(process.env.NODE_ENV === 'development') {
 		// Open the DevTools.
-		mainWindow.webContents.openDevTools();
+		win.webContents.openDevTools();
 		
+		/*
 		installExtension(REACT_DEVELOPER_TOOLS)
 			.then((name) => console.log(`Added Extension:  ${name}`))
-			.catch((err) => console.log('An error occurred: ', err));
+			.catch((err) => console.log('An error occurred: ', err));*/
 
   }
 
   // Emitted when the window is closed.
-  mainWindow.on('closed', function() {
+  win.on('closed', function() {
 	// Dereference the window object, usually you would store windows
 	// in an array if your app supports multi windows, this is the time
 	// when you should delete the corresponding element.
-	mainWindow = null;
+	win = null;
   });
 }
 
@@ -65,7 +88,7 @@ app.on('window-all-closed', function() {
 app.on('activate', function() {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
+  if (win === null) {
 		createWindow();
   }
 });
