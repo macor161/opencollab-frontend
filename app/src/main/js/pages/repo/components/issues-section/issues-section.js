155c3fffcc633168cfa6340393300148c024ef63
Mathew Cormier <mathew.corm@gmail.com>
2018-03-13 22:14:52 -0400

    "Add issue" button

--- app/src/main/js/pages/repo/components/issues-section/issues-section.js
+++ app/src/main/js/pages/repo/components/issues-section/issues-section.js
@@ -1,22 +1,32 @@
 import React, { Component } from 'react'
 import { Collection, CollectionItem } from 'react-materialize'
-
+import { Link } from 'react-router-dom'
 
 import { IssuesStore } from './issues-store'
 
+import './issues-sections.css'
+
+
+const Header = (repo) => 
+  <div className="issues-header">
+    <h4>Active issues</h4>
+    <Link className="add-link" to={`/repo/${repo}/issues/new`}>+</Link>
+  </div>
+
+
 export class IssuesSection extends Component {
 
-  
   
     constructor(props) {
       super(props)
       this.store = new IssuesStore(props.repo)
     }
+   
   
     render() {
       return (
-        <div className="code section">
-            <Collection header="Active issues">
+        <div className="section issues">
+            <Collection header={<Header repo={this.props.repo}/>}>
               {this.store.issues.map(issue => 
                 <CollectionItem href="#" key={issue.id}>{issue.name}</CollectionItem>
               )}
