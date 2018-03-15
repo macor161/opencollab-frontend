ae0a9c15851870c2cb465efca04852dbbdd9bf14
Mathew Cormier <mathew.corm@gmail.com>
2018-03-14 21:45:59 -0400

    Add new-issue form items

--- app/src/main/js/pages/repo/components/new-issue-section/new-issue-section.js
+++ app/src/main/js/pages/repo/components/new-issue-section/new-issue-section.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react'
-import { Collection, CollectionItem } from 'react-materialize'
+import { Collection, CollectionItem, Button, Input, Preloader } from 'react-materialize'
 import { Link } from 'react-router-dom'
+import { observer } from 'mobx-react'
 
 import { NewIssueStore } from './new-issue-store'
 
 import './new-issue-section.css'
 
 
-
+@observer
 export class NewIssueSection extends Component {
 
   
@@ -20,7 +21,25 @@ export class NewIssueSection extends Component {
     render() {
       return (
         <div className="section new-issue">
-          new issue
+          <h2>New Issue</h2>
+          <br />
+          <Input 
+              label="Issue name" 
+              value={this.store.issue.name} 
+              onChange={e => this.store.issue.name = e.target.value } 
+          />
+          <br />
+          <Input 
+              type='textarea'
+              label="Issue description" 
+              value={this.store.issue.description} 
+              onChange={e => this.store.issue.description = e.target.value } 
+          />
+          <br />
+          <div className="btn-container">
+              <Button waves='light' className="create-btn" onClick={() => this.store.createRepo()}>Create issue</Button> 
+              <Button className="cancel lighten-5">Cancel</Button>
+          </div>          
         </div>
       )
     }
