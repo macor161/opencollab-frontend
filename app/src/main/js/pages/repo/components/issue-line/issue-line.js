132c18290c56a8e92d1358116ea938eeef242b46
Mathew Cormier <mathew.corm@gmail.com>
2018-03-17 19:49:18 -0400

    Add tokens and vote button

--- app/src/main/js/pages/repo/components/issue-line/issue-line.js
+++ app/src/main/js/pages/repo/components/issue-line/issue-line.js
@@ -9,12 +9,18 @@ export class IssueLine extends Component {
     
   
     render = () =>
-      <CollectionItem className="issue">
+      <CollectionItem className="issue-line">
         <div className="id">#{this.props.issue.id + 1}</div>
         <div className="main-content">
           <div className="name">{this.props.issue.name}</div>
           <div className="desc">{this.props.issue.description}</div>
         </div>
+        <div className="staked-tokens">
+          <strong>{this.props.issue.totalStake.toString()}</strong> tokens
+        </div>
+        <div className="edit-stake">
+          <img className="edit-stake-btn" src={require('./images/vote.svg')} />
+        </div>        
       </CollectionItem>
   
   }
