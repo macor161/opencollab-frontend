4a31e65ae0fe8623db995034f63c94a608fcd375
Mathew Cormier <mathew.corm@gmail.com>
2018-03-18 17:26:37 -0400

    new repo dropdown

--- app/src/main/js/components/header/header.js
+++ app/src/main/js/components/header/header.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import { Dropdown } from 'semantic-ui-react'
 import { observer } from 'mobx-react'
+
+
 import { headerStore } from './header-store'
 import { repoStore } from '../../pages/repo/repo-page-store'
 
@@ -18,25 +21,28 @@ class Header extends Component {
 		return repoStore.availableTokensString
 	}
 
-	render(){
-		return(
-			<div className="header">
-				<div className="container">
-					<Link to="/" className="main-logo">OpenCollab {/*Frontend*/}</Link>
+	render = () =>
+    <div className="header">
+      <div className="container">
+        <Link to="/" className="main-logo">OpenCollab {/*Frontend*/}</Link>
 
-					<div className="main-menu">
-						{ this.tokensVisible() &&
-							<div className="available-tokens">
-								<strong>{repoStore.availableTokensString}</strong> tokens
-							</div>
-						}					
-						<Link to="/new-repo" className="plus">+</Link>
-						<Link to="/" className="user" />
-					</div>
-				</div>
-			</div>
-		)
-	}
+        <div className="main-menu">
+          { this.tokensVisible() &&
+            <div className="available-tokens">
+              <strong>{repoStore.availableTokensString}</strong> tokens
+            </div>
+          }	
+          <Dropdown text='+' className="new-repo-btn">
+            <Dropdown.Menu>
+            <Dropdown.Item><Link to="/new-repo">Create a new repository</Link></Dropdown.Item>
+            <Dropdown.Item><Link to="/import-github-repo">Import from GitHub</Link></Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>				
+          <Link to="/" className="user" />
+        </div>
+      </div>
+    </div>
+
 }
 
 export default Header
