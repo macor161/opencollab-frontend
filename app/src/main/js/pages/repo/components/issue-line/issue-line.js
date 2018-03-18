80b8b921baa72eb9daf5a46b4d3a8e0d8bbf4a8c
Mathew Cormier <mathew.corm@gmail.com>
2018-03-17 20:55:37 -0400

    Add voting buttons

--- app/src/main/js/pages/repo/components/issue-line/issue-line.js
+++ app/src/main/js/pages/repo/components/issue-line/issue-line.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react'
 import { Collection, CollectionItem } from 'react-materialize'
+import { default as c } from 'classnames'
 
 import './issue-line.css'
 
 export class IssueLine extends Component {
 
-    state = { tokensToStake: 0 }
+    state = { 
+      tokensToStake: 0, 
+      isVoting: false 
+    }
+
+
+    startVote = () => this.setState({ isVoting: true })
     
   
     render = () =>
@@ -18,8 +25,30 @@ export class IssueLine extends Component {
         <div className="staked-tokens">
           <strong>{this.props.issue.totalStake.toString()}</strong> tokens
         </div>
-        <div className="edit-stake">
-          <img className="edit-stake-btn" src={require('./images/vote.svg')} />
+        <div className={c("edit-stake", { 'voting': this.state.isVoting })}>
+          <img 
+            className="edit-stake-btn" 
+            src={require('./images/vote.svg')} 
+            onClick={this.startVote}
+          />
+          <div className="voting-buttons">
+            <div className="arrows">
+              <img className="arrow-up" src={require('./images/arrow-up.svg')} />
+              <img className="arrow-down" src={require('./images/arrow-down.svg')} />
+            </div>
+
+            <img 
+              className="ok-btn" 
+              src={require('./images/ok.png')} 
+              onClick={this.startVote}
+            />
+
+            <img 
+              className="cancel-btn" 
+              src={require('./images/cancel.png')} 
+              onClick={this.startVote}
+            />            
+          </div>
         </div>        
       </CollectionItem>
   
