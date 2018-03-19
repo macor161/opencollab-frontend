451d6b2f2c9eaf96476a289830b080bb13945229
Mathew Cormier <mathew.corm@gmail.com>
2018-03-18 20:49:30 -0400

    Add modal

--- app/src/main/js/pages/import-github-repo/import-github-repo.js
+++ app/src/main/js/pages/import-github-repo/import-github-repo.js
@@ -20,78 +20,81 @@ class ImportGithubRepoPage extends Component {
     window.importGithubRepoStore = this.store
   }
 
-  render() {
-    return (
-      <div className={c("new-repo", "page", { "loading": this.store.isLoading })}>
-        <div className="container-md">
-          <h2>Import a repository from GitHub</h2>
-          <p className="subhead-description">
-            A repository contains all the files for your project, including the revision history.
-          </p>
+  render = () =>
+    <div className={c("new-repo-import", "page", { "loading": this.store.isLoading })}>
+      <div className="container-md">
+        <h2>Import a repository from GitHub</h2>
+        <p className="subhead-description">
+          A repository contains all the files for your project, including the revision history.
+        </p>
 
 
-          <Modal trigger={<Button className="repo-select-btn">Select repository</Button>}>
-            <Modal.Header>Select your GitHub Repository</Modal.Header>
-            <Modal.Content>
-              <Modal.Description>
-                <p>We've found the following gravatar image associated with your e-mail address.</p>
-                <p>Is it okay to use this photo?</p>
-              </Modal.Description>
-            </Modal.Content>
-          </Modal>          
+        <Modal className="repo-import-modal" trigger={<Button className="repo-select-btn">Select repository</Button>}>
+          <Modal.Header>Select your GitHub Repository</Modal.Header>
+          <Modal.Content>
+            <Modal.Description>
+              <p>Enter the repo URL:</p>
+              <Input
+                placeholder="ex: https://www.github.com/macor161/opencollab-frontend"
+                className="repo-url"
+              />
 
-          <br />
+              <div className="btn-container">
+                <Button waves='light' className="create-btn" onClick={() => this.store.importBasicInfo()}>OK</Button>
+                <Button className="cancel lighten-5">Cancel</Button>
+              </div>          
+            </Modal.Description>
+          </Modal.Content>
+        </Modal>          
 
-          <Input
-            label="Repository Name"
-            value={this.store.name}
-            onChange={e => this.store.name = e.target.value}
-          />
+        <br />
 
-          <Input
-            label="Repository Description"
-            value={this.store.description}
-            onChange={e => this.store.description = e.target.value}
-          />
+        <Input
+          label="Repository Name"
+          value={this.store.name}
+          onChange={e => this.store.name = e.target.value}
+        />
 
-          <br />
+        <Input
+          label="Repository Description"
+          value={this.store.description}
+          onChange={e => this.store.description = e.target.value}
+        />
 
-          <Input
-            label="Number of tokens to create"
-            type="number"
-            className="token-amount"
-            value={this.store.tokenAmount}
-            onChange={e => this.store.tokenAmount = parseInt(e.target.value || 0)}
-          />
+        <br />
 
-          <Input
-            label="Voter reward percentage"
-            type="number"
-            className="voter-reward-pct"
-            value={this.store.voterRewardPercentage}
-            onChange={e => this.store.voterRewardPercentage = parseFloat(e.target.value || 0)}
-          />
+        <Input
+          label="Number of tokens to create"
+          type="number"
+          className="token-amount"
+          value={this.store.tokenAmount}
+          onChange={e => this.store.tokenAmount = parseInt(e.target.value || 0)}
+        />
 
-          <Input
-            label="Voter penalty percentage"
-            type="number"
-            className="voter-penalty-pct"
-            value={this.store.voterPenaltyPercentage}
-            onChange={e => this.store.voterPenaltyPercentage = parseFloat(e.target.value || 0)}
-          />
+        <Input
+          label="Voter reward percentage"
+          type="number"
+          className="voter-reward-pct"
+          value={this.store.voterRewardPercentage}
+          onChange={e => this.store.voterRewardPercentage = parseFloat(e.target.value || 0)}
+        />
 
-          <br />
+        <Input
+          label="Voter penalty percentage"
+          type="number"
+          className="voter-penalty-pct"
+          value={this.store.voterPenaltyPercentage}
+          onChange={e => this.store.voterPenaltyPercentage = parseFloat(e.target.value || 0)}
+        />
 
-
-          <div className="btn-container">
-            <Button waves='light' className="create-btn" onClick={() => this.store.createRepo()}>Import repository</Button>
-            <Button className="cancel lighten-5">Cancel</Button>
-          </div>
+        <div className="btn-container">
+          <Button waves='light' className="create-btn" onClick={() => this.store.createRepo()}>Import repository</Button>
+          <Button className="cancel lighten-5">Cancel</Button>
         </div>
-        <div className="loading-container"><Preloader /></div>
       </div>
-    )
-  }
+      <div className="loading-container"><Preloader /></div>
+    </div>
+
 }
 
 export default ImportGithubRepoPage
